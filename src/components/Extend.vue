31daa9814bd5ba3c3ccbb0f5df2d998cea69d6b4
浮烟 <rx6@qq.com>
2020-05-26 11:54:19 +0800

    Initial commit

---
+++ src/components/Extend.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="m-extend">
+        <!-- <ins
+            class="adsbygoogle"
+            style="display:block;width:100%;overflow:hidden;"
+            data-ad-client="ca-pub-4388499329141185"
+            data-ad-slot="9773281887"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+        ></ins> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Extend",
+    props: [],
+    data: function() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    mounted: function() {},
+};
+</script>
+
+<style lang="less"></style>
