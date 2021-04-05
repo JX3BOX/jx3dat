f912143290f19a0cb0535ca90b94986dcf1ec9de
iRuxu <rx6@qq.com>
2021-04-05 11:58:20 +0800

    cms2.0

---
+++ src/Post.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <Breadcrumb
+            name="插件数据"
+            slug="jx3dat"
+            root="/jx3dat"
+            :publishEnable="true"
+            :adminEnable="true"
+            :feedbackEnable="true"
+            :crumbEnable="true"
+        >
+            <img slot="logo" svg-inline src="./assets/img/jx3dat.svg" />
+        </Breadcrumb>
+        <LeftSidebar>
+            <Nav class="m-nav" />
+        </LeftSidebar>
+        <Main :withoutRight="false">
+            <single />
+            <RightSidebar>
+                <Side class="m-extend" />
+            </RightSidebar>
+            <Footer></Footer>
+        </Main>
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/single_nav.vue";
+import Side from "@/components/single_side.vue";
+import single from "@/components/single.vue";
+export default {
+    name: "App",
+    props: [],
+    data: function () {
+        return {};
+    },
+    methods: {},
+    components: {
+        Nav,
+        Side,
+        single,
+    },
+    beforeCreate : function (){
+        
+    }
+};
+</script>
+
+<style lang="less">
+@import "./assets/css/layout.less";
+</style>
