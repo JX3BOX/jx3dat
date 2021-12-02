194897a5f840c56fc8ba42dd53e19e84412d5a24
iRuxu <rx6@qq.com>
2021-12-03 04:11:52 +0800

    fix:redirect index to dbm

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import Plugins from "../views/Plugins.vue";
 Vue.use(VueRouter);
 
 const routes = [
-    { name: "index", path: "/", redirect: "/userdata" },
+    { name: "index", path: "/", redirect: "/plugins/1" },
     { name: "userdata", path: "/userdata", component: Userdata },
     { name: "plugins", path: "/plugins/:subtype", component: Plugins },
     { name: "rank", path: "/rank", component: Rank, meta: { belongs: "dbm" } },
